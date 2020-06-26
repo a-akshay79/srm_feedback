705c81867f3be604c980a191f59d29ec4e8749d4
Akshay Anand <a.akshay7799@gmail.com>
2020-06-26 13:25:56 +0530

    0.2

--- srm_feedback.py
+++ srm_feedback.py
@@ -9,41 +9,33 @@ import platform
 
 username = input('Enter Username:')
 password = input('Enter Password:')
+
 if platform.system() == 'Windows':
     browser = webdriver.Chrome(ChromeDriverManager().install())
 else:
     browser = webdriver.Chrome()
+
 browser.get("https://academia.srmist.edu.in")
 browser.switch_to.frame('zohoiam')
-userelem = browser.find_element_by_css_selector('#Email')
-userelem.send_keys(username)
-passelem = browser.find_element_by_css_selector('#Password')
-passelem.send_keys(password)
-loginelem = browser.find_element_by_css_selector('#signinForm > div:nth-child(6) > input')
-loginelem.click()
+browser.find_element_by_css_selector('#Email').send_keys(username)
+browser.find_element_by_css_selector('#Password').send_keys(password)
+browser.find_element_by_css_selector('#signinForm > div:nth-child(6) > input').click()
 time.sleep(10)
-courseelem = browser.find_element_by_css_selector('#zc-container > tbody > tr:nth-child(1) > td > div.zc-menutabdiv > table > tbody > tr > td:nth-child(6)')
-courseelem.click()
+browser.find_element_by_css_selector('#zc-container > tbody > tr:nth-child(1) > td > div.zc-menutabdiv > table > tbody > tr > td:nth-child(6)').click()
 time.sleep(15)
 drop = browser.find_elements_by_class_name('search-selected-val')
 
 perf = ['Average','Excellent','Good','Poor','Very Good']
-for i in drop:
-    try:
-        i.click()
-        for _ in range(random.randint(0,5)):
-            pyautogui.press('down')
-        pyautogui.press('enter')
-    except:
-        pass
 
-for i in drop:
-    try:
-        i.click()
-        pyautogui.press('down')
-        pyautogui.press('enter')
-    except:
-        pass
+for i in range(5):
+    for i in drop:
+        try:
+            i.click()
+            for _ in range(random.randint(0,5)):
+                pyautogui.press('down')
+            pyautogui.press('enter')
+        except:
+            pass
 
 comments = browser.find_elements_by_class_name('zc-gridsubform-input')
 for i in comments:
